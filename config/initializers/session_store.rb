ab26b1c0135dc3620b7221a7ee93935f16565dc7
awead <amsterdamos@gmail.com>
2011-06-23 11:51:43 -0400

    move to rails3

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,15 +1,8 @@
 # Be sure to restart your server when you modify this file.
 
-# Your secret key for verifying cookie session data integrity.
-# If you change this key, all old sessions will become invalid!
-# Make sure the secret is at least 30 characters and all random, 
-# no regular words or you'll be exposed to dictionary attacks.
-ActionController::Base.session = {
-  :key         => '_at-report_session',
-  :secret      => '282797d701246036ad1c74e8e272493ea11a5d421bca1e90cbcf4eb1f95bc8f629857acf22a2f059ad19d5b74170a378f620a790e449a9456aaed172c90220d9'
-}
+AtReport::Application.config.session_store :cookie_store, :key => '_at-report_session'
 
 # Use the database for sessions instead of the cookie-based default,
 # which shouldn't be used to store highly confidential information
-# (create the session table with "rake db:sessions:create")
-# ActionController::Base.session_store = :active_record_store
+# (create the session table with "rails generate session_migration")
+# AtReport::Application.config.session_store :active_record_store
