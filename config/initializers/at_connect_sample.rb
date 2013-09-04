edbe10d447b7beacc094f9aa76f349b62682e5bb
Adam Wead <amsterdamos@gmail.com>
2013-09-04 13:37:57 -0400

    Adding sample dbase connection config

---
+++ config/initializers/at_connect_sample.rb
@@ -0,0 +1,11 @@
+# sample connection configuration.  Rename to at_connect.rb 
+#
+# We override ActiveRecord to connect to our AT Database
+#ActiveRecord::Base.establish_connection(
+#  :adapter  => 'mysql',
+#  :encoding => 'utf8',
+#  :database => 'at_dbase_name',
+#  :host     => 'host.example.com',
+#  :username => 'atuser',
+#  :password => 'plain text password'
+#)
